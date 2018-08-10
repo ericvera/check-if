01d6e6d407fc469a5f2ddb7c23bb742fb4fd9020
Eric Vera <ev@Erics-MacBook.local>
2018-08-10 12:51:20 -0300

    Add containsUpdatedProperties

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const containsProperties = require('./lib/containsProperties')
+const containsUpdatedProperties = require('./lib/containsUpdatedProperties')
 const isDateInMs = require('./lib/isDateInMs')
 const isFirestoreId = require('./lib/isFirestoreId')
 const isShortDate = require('./lib/isShortDate')
@@ -6,6 +7,7 @@ const isTrimmedNonEmptyString = require('./lib/isTrimmedNonEmptyString')
 
 module.exports = {
   containsProperties,
+  containsUpdatedProperties,
   isDateInMs,
   isFirestoreId,
   isShortDate,
