6f1974b525457767d075dad4aa91fa0131551806
Eric Vera <ev@Erics-MacBook.local>
2018-08-10 13:55:46 -0300

    Add a few additional functions

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const areInSync = require('./lib/areInSync')
 const containsProperties = require('./lib/containsProperties')
 const containsUpdatedProperties = require('./lib/containsUpdatedProperties')
 const isDateInMs = require('./lib/isDateInMs')
 const isFirestoreId = require('./lib/isFirestoreId')
+const isNonDeletedObject = require('./lib/isNonDeletedObject')
+const isNonEmptyObject = require('./lib/isNonEmptyObject')
 const isShortDate = require('./lib/isShortDate')
 const isTrimmedNonEmptyString = require('./lib/isTrimmedNonEmptyString')
 
 module.exports = {
+  areInSync,
   containsProperties,
   containsUpdatedProperties,
   isDateInMs,
   isFirestoreId,
+  isNonDeletedObject,
+  isNonEmptyObject,
   isShortDate,
   isTrimmedNonEmptyString
 }
